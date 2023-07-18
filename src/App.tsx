939e30f7d29486763c0982ac35c3602d2fdd1b85
Joy Fan <chandler0204@gmail.com>
2023-07-18 14:31:03 +0800

    asset: add svg picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useCallback } from 'react'
 import NameInput from 'component/NameInput'
 import ChatBox from 'component/ChatBox'
+import ChatAnimation from 'component/ChatAnimation'
 import { Box } from '@mui/material'
 import SocketContext, { socket } from 'context/SocketContext'
 import UserNameContext from 'context/UserNameContext'
@@ -13,7 +14,8 @@ function App() {
   return (
     <SocketContext.Provider value={socket}>
       <UserNameContext.Provider value={name}>
-        <Box sx={{ height: '100vh', minHeight: 300 }}>
+        <Box sx={{ height: '100vh', minHeight: 300, overflow: 'hidden' }}>
+          <ChatAnimation />
           <NameInput changeName={changeName} />
           {name && <ChatBox />}
         </Box>
